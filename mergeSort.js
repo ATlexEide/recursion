28c78be994b57f98f1fdd3c804069f473883ca46
Alexander Eide <atlexeide@gmail.com>
2024-07-26 13:17:13 +0200

    changes

--- mergeSort.js
+++ mergeSort.js
@@ -1,21 +1,41 @@
 let array = [4, 2, 7, 5, 0, 1, 3, 6]
-let tempArray = [];
+let temp = [];
+let arrtemp = [];
 
 function sort(array) {
-    if (array.length === 1) {
-        console.log(`single: ${array}`)
-        return array
-    } else {
-        tempArray.push(sort(array.splice(array.length / 2)), sort(array));
-    }
+    if (array.length === 1)
+        return array;
+    let left = array.slice(0, array.length / 2);
+    let right = array.slice(array.length / 2);
+    merge(sort(left), sort(right));
 }
-function merge(a, b) {
-    if (a || b) {
-        console.log('a: ' + a)
-        console.log('b: ' + b)
-        if (a > b)
-            array.push(a)
-        console.log(array)
-    }
-}
+
+function merge(left, right) {
+    let leftIndex = 0;
+    let rightIndex = 0;
+    while (leftIndex <= left.length && rightIndex <= right.length) {
+        if (left[leftIndex] === undefined) {
+            temp.push(right[rightIndex]);
+            leftIndex++;
+        }
+        else if (right[rightIndex] === undefined) {
+            temp.push(left[leftIndex]);
+            rightIndex++;
+        }
+        else if (left[leftIndex] < right[rightIndex]) {
+            temp.push(left[leftIndex]);
+            leftIndex++;
+            console.log('left')
+            console.log(temp);
+        } else {
+            temp.push(right[rightIndex]);
+            rightIndex++;
+            console.log('right')
+            console.log(temp);
+        };
+    };
+};
 sort(array)
+
+
+
